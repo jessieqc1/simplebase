1a87feb050def95d5a148a0e679353611ecf5db9
jessieqc1 <156117682+jessieqc1@users.noreply.github.com>
2024-10-01 17:18:47 -1000

    added toggle menu

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// TOGGLE MENU
+const toggleMenu = document.querySelector('.toggle-menu');
+const toggleMenuButton = document.querySelector('.site-menu-button');
+toggleMenuButton.onclick = function() {
+    if (toggleMenu.getAttribute('data-menustate') === 'closed') {
+      // if closed, open it    
+      toggleMenu.setAttribute('data-menustate','open');
+    } else {
+      // else, close it
+      toggleMenu.setAttribute('data-menustate','closed');
+    }
+};
+
+console.log(toggleMenu);
+console.log(toggleMenuButton);
