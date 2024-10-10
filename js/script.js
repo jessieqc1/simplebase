e0e34f9fbfdc97f555909d805a206aaac383ba27
jessieqc1 <156117682+jessieqc1@users.noreply.github.com>
2024-10-09 23:02:59 -1000

    animated menu icon, added javascript

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 // TOGGLE MENU
 const toggleMenu = document.querySelector('.toggle-menu');
 const toggleMenuButton = document.querySelector('.site-menu-button');
+const toggleMenuLinks = document.querySelectorAll('.toggle-menu-a');
 toggleMenuButton.onclick = function() {
     if (toggleMenu.getAttribute('data-menustate') === 'closed') {
       // if closed, open it    
@@ -11,6 +12,10 @@ toggleMenuButton.onclick = function() {
       toggleMenu.setAttribute('data-menustate','closed');
     }
 };
-
-console.log(toggleMenu);
-console.log(toggleMenuButton);
+// CLOSE THE MENU WHEN A USER CLICKS ON AY LINK
+toggleMenuLinks.forEach((el) => {
+  el.onclick = () => {
+    toggleMenu.setAttribute('data-menustate','closed');
+  }
+}
+);
