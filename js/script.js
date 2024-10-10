8426ec34f34d246450e87209f2d216a1c8e0484e
jessieqc1 <156117682+jessieqc1@users.noreply.github.com>
2024-10-10 12:49:35 -1000

    completed simple base+

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 // TOGGLE MENU
 const toggleMenu = document.querySelector('.toggle-menu');
 const toggleMenuButton = document.querySelector('.site-menu-button');
-const toggleMenuLinks = document.querySelectorAll('.toggle-menu-a');
 toggleMenuButton.onclick = function() {
     if (toggleMenu.getAttribute('data-menustate') === 'closed') {
       // if closed, open it    
@@ -12,10 +11,10 @@ toggleMenuButton.onclick = function() {
       toggleMenu.setAttribute('data-menustate','closed');
     }
 };
-// CLOSE THE MENU WHEN A USER CLICKS ON AY LINK
+
+const toggleMenuLinks = document.querySelectorAll('.toggle-menu a');
 toggleMenuLinks.forEach((el) => {
   el.onclick = () => {
     toggleMenu.setAttribute('data-menustate','closed');
   }
-}
-);
+});
